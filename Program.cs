2beac8f40dcdfdc3af6037b97cd59cb5bbccb3ab
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-05-13 13:53:23 -0500

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        List<Quarto> ListadeQuatos = new List<Quarto>();
+
+        Console.WriteLine("_Menu_");
+        Console.WriteLine("1- Criar quarto");
+        Console.WriteLine("2- Reservar quarto");
+        Console.WriteLine("3- Cancelar reserva");
+        Console.WriteLine("4- Calcular valor da estadia");
+        Console.WriteLine("5- Exibir dados do quarto");
+        Console.WriteLine("6- Sair");
+
+
+        static Quarto BuscarQuarto (int numeroquarto, List<Quarto> lista)
+        {
+        foreach(var item in lista)
+            {
+                if(item.Numero == numeroquarto)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        int numero = 6;
+        switch (numero)
+        {
+            case 1:
+
+        }
+
+
+    }
+}
---
+++ quarto.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;       
+
+class Quarto
+{
+    private int numero;
+    private string nome = "";
+    private decimal valordiaria;
+    private bool ocupado;
+
+    public int Numero 
+    { 
+        get { return numero; }
+        set { if (value > 0) numero = value; }
+    }
+
+    public string Nome 
+    { 
+        get { return nome; }
+        set { nome = value ?? ""; }
+    }
+
+    public decimal ValorDiaria 
+    { 
+        get { return valordiaria; }
+        set { if (value >= 0) valordiaria = value; }
+    }
+
+    public bool Ocupado 
+    { 
+        get { return ocupado; }
+        set { ocupado = value; }
+    }
+
+    public void Reserva(string NomeHospede)
+    {
+        if (ocupado)
+            throw new InvalidOperationException($"O quarto {numero} já está ocupado!");
+        
+        
+        Console.WriteLine($"Quarto {numero} reservado para {NomeHospede}");
+
+        this.Nome = NomeHospede;
+        this.Ocupado = true;
+        Console.WriteLine($"Reserva realizada para {NomeHospede}");
+    }
+
+    public void CancelarReserva()
+    {
+        if(!Ocupado)
+        {
+            throw new InvalidOperationException("Não é possível cancelar: o quarto já está livre!");
+        }
+
+        Ocupado = false;
+        Nome = "";
+        Console.WriteLine("Reserva cancelada com sucesso!");
+    }
+    public decimal CalcularValor(int dias)
+    {
+        if(dias > 0)
+        {
+            throw new ArgumentException("A quantidade de dias tem que ser maior que zero");
+        }
+        return dias * valordiaria;
+    }
+    
+}
